14629fc9405645ef73220d3ea4fcb5a0c05aa1bf
André Sobreira <andresobreira@live.com>
2016-05-22 14:29:45 -0300

    Separação JS/CSS/HTML
    Separação das partes de JavaScript e CSS de dentro do html do Banner.

---
+++ Site de Vendas/js/rotinaBanner.js
@@ -0,0 +1,39 @@
+	var counter=1;
+	var intervalId=null;
+	var totalimg=0;
+	var imgsrc="a.jpg";
+	var imglink="#";
+	function myFunction(){
+	
+	intervalId= setInterval(bip, 30);
+	
+	}
+	function bip(){
+	  var imgobj= document.getElementById("banner");//criando o objeto do elemento banner para substituir a image no correr do tempo
+	  var imglinkh=document.getElementById("bannerclick");//criando o objeto do elemento bannerclick para substituir o  link no correr do  tempo
+	  
+	  document.getElementById("bip").innerHTML= totalimg+1;//usado para contar o total de imagens no banner
+	  imgobj.src=imgsrc;//substitui a image
+	  imglinkh.href= imglink;//substitui o link
+	  
+	  counter=counter+1;
+	  
+	  if(counter==100){//quando o valor chegar a 100 muda a imagem
+	     counter=1;
+		 totalimg=totalimg+1;
+		 if(totalimg==1){
+		    imgsrc="b.jpg";
+			imglink="#";
+		  }
+		  else if(totalimg==2){
+		    imgsrc="c.jpg";
+			imglink="#";
+		
+		   }
+		  else if(totalimg==3){
+		    imgsrc="d.jpg";
+			imglink="#";
+			totalimg=0;
+		  }
+	  }
+	}

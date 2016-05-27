ddef90b9d639e9e56ea5106af71861bea8e71d1f
Romenito <romen_damaceno@hotmail.com>
2016-05-27 01:22:38 -0300

    Alto Contraste

---
+++ Site de Vendas/js/altocontraste.js
@@ -0,0 +1,111 @@
+
+var temp=1;
+function mudaCor(i) {
+	
+if(temp==i){
+ document.getElementById("interface").style.backgroundColor = "black";
+ document.getElementById("interface").style.color = "white";
+ document.getElementById("corpo").style.backgroundColor = "white";
+ document.getElementById("rodape").style.backgroundColor="black";
+ document.getElementById("cabecalho").style.backgroundColor = "black";
+ document.getElementById("rodape").style.color = "white";
+ document.getElementById("menu1").style.backgroundColor  = "black";
+ document.getElementById("menu1").style.textDecoration = "underline";
+ document.getElementById("menu2").style.backgroundColor  = "black";
+ document.getElementById("menu2").style.textDecoration = "underline";
+ document.getElementById("menu3").style.backgroundColor  = "black";
+ document.getElementById("menu3").style.textDecoration = "underline";
+ document.getElementById("menu4").style.backgroundColor  = "black";
+ document.getElementById("menu4").style.textDecoration = "underline";
+ document.getElementById("menu5").style.backgroundColor  = "black";
+ document.getElementById("menu5").style.textDecoration = "underline";
+ document.getElementById("menu1.1").style.color  = "#ffff1a";
+ document.getElementById("menu1.2").style.color  = "#ffff1a";
+ document.getElementById("menu1.3").style.color  = "#ffff1a";
+ document.getElementById("menu1.4").style.color  = "#ffff1a";
+ document.getElementById("menu1.5").style.color  = "#ffff1a";
+ document.getElementById("link-conteudo").style.backgroundColor  = "black";
+ document.getElementById("link-conteudo").style.textDecoration = "underline";
+ document.getElementById("link-conteudo").style.color  = "#ffff1a";
+ document.getElementById("link-busca").style.backgroundColor  = "black";
+ document.getElementById("link-busca").style.textDecoration = "underline";
+ document.getElementById("link-busca").style.color  = "#ffff1a";
+ document.getElementById("link-rodape").style.backgroundColor  = "black";
+ document.getElementById("link-rodape").style.textDecoration = "underline";
+ document.getElementById("link-rodape").style.color  = "#ffff1a";
+ document.getElementById("link-alto-contraste").style.backgroundColor  = "black";
+ document.getElementById("link-alto-contraste").style.textDecoration = "underline";
+ document.getElementById("link-alto-contraste").style.color  = "#ffff1a";
+ document.getElementById("link-menu").style.backgroundColor  = "black";
+ document.getElementById("link-menu").style.textDecoration = "underline";
+ document.getElementById("link-menu").style.color  = "#ffff1a";
+ document.getElementById("link-mapa-site").style.backgroundColor  = "black";
+ document.getElementById("link-mapa-site").style.textDecoration = "underline";
+ document.getElementById("link-mapa-site").style.color  = "#ffff1a";
+ document.getElementById("cadastrar").style.backgroundColor  = "black";
+ document.getElementById("cadastrar").style.textDecoration = "underline";
+ document.getElementById("cadastrar").style.color  = "#ffff1a";
+ document.getElementById("login").style.color  = "black";
+ document.getElementById("nome-empresa").style.color = "black";
+ document.getElementById("slogan").style.color  = "black";
+
+ temp=2;
+
+}
+
+else {
+ document.getElementById("interface").style.backgroundColor = "white";
+ document.getElementById("interface").style.color = "black";
+ document.getElementById("corpo").style.backgroundColor = "#009933";
+ document.getElementById("rodape").style.backgroundColor="#00cc44";
+ document.getElementById("cabecalho").style.backgroundColor = "white";
+ document.getElementById("rodape").style.color = "black";
+ document.getElementById("menu1").style.backgroundColor  = "#00cc44";
+ document.getElementById("menu1").style.textDecoration = "none";
+ document.getElementById("menu2").style.backgroundColor  = "#00cc44";
+ document.getElementById("menu2").style.textDecoration = "none";
+ document.getElementById("menu3").style.backgroundColor  = "#00cc44";
+ document.getElementById("menu3").style.textDecoration = "none";
+ document.getElementById("menu4").style.backgroundColor  = "#00cc44";
+ document.getElementById("menu4").style.textDecoration = "none";
+ document.getElementById("menu5").style.backgroundColor  = "#00cc44";
+ document.getElementById("menu5").style.textDecoration = "none";
+ document.getElementById("menu1.1").style.color  = "black";
+ document.getElementById("menu1.2").style.color  = "black";
+ document.getElementById("menu1.3").style.color  = "black";
+ document.getElementById("menu1.4").style.color  = "black";
+ document.getElementById("menu1.5").style.color  = "black";
+ document.getElementById("link-conteudo").style.backgroundColor  = "#00cc44";
+ document.getElementById("link-conteudo").style.textDecoration = "none";
+ document.getElementById("link-conteudo").style.color = "black";
+ document.getElementById("link-busca").style.backgroundColor  = "#00cc44";
+ document.getElementById("link-busca").style.textDecoration = "none";
+ document.getElementById("link-busca").style.color = "black";
+ document.getElementById("link-rodape").style.backgroundColor  = "#00cc44";
+ document.getElementById("link-rodape").style.textDecoration = "none";
+ document.getElementById("link-rodape").style.color = "black";
+ document.getElementById("link-alto-contraste").style.backgroundColor  = "#00cc44";
+ document.getElementById("link-alto-contraste").style.textDecoration = "none";
+ document.getElementById("link-alto-contraste").style.color = "black";
+ document.getElementById("link-menu").style.backgroundColor  = "#00cc44";
+ document.getElementById("link-menu").style.textDecoration = "none";
+ document.getElementById("link-menu").style.color = "black";
+ document.getElementById("link-mapa-site").style.backgroundColor  = "#00cc44";
+ document.getElementById("link-mapa-site").style.textDecoration = "none";
+ document.getElementById("link-mapa-site").style.color = "black";
+ document.getElementById("cadastrar").style.backgroundColor  = "#00cc44";
+ document.getElementById("cadastrar").style.textDecoration = "none";
+ document.getElementById("cadastrar").style.color = "black";
+ document.getElementById("login").style.color  = "black";
+ document.getElementById("nome-empresa").style.color = "#00cc44";
+ document.getElementById("slogan").style.color  = "#00cc44";
+ temp=1;
+}
+
+
+
+
+
+
+}
+ 
